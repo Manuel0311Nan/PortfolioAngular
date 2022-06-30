8d9356acf9713a27fdd4c570b9af1997994661fd
Manuel Alcázar <manuzz3112@gmail.com>
2022-06-30 13:36:05 +0200

    maquetación 2

--- src/app/models/user-register.ts
+++ src/app/models/user-register.ts
@@ -1,5 +1,4 @@
 export interface UserRegister {
   name: string;
   email: string;
-  password: string;
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -15,7 +15,6 @@ export class ContactComponent{
     this.userRegisterForm = this.formBuilder.group({
       name:  new FormControl( '', [Validators.required,]),
       email:new FormControl( '', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
-      password: new FormControl( '', [Validators.required,  Validators.pattern("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$")])
     })
   }
   public onSubmit(): void {
@@ -24,7 +23,6 @@ export class ContactComponent{
       const user: UserRegister = {
         name: this.userRegisterForm.get('name')?.value,
         email: this.userRegisterForm.get('email')?.value,
-        password: this.userRegisterForm.get('password')?.value,
       };
       console.log(user);
       this.userRegisterForm.reset();
